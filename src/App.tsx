139db72b5aee4e4ff8c833264014e2bd2a1c6812
Lil-El <yxd99324@qq.com>
2021-09-04 19:22:17 +0800

    react

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter } from 'react-router-dom'
+import AppScheduler from './views/AppScheduler'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppScheduler />
+    </BrowserRouter>
+  )
+}
+
+export default App
