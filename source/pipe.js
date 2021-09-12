6983254550eb556c2c6d35283f261a5cf5a86dc2
Lil-El <yxd99324@qq.com>
2021-09-12 20:53:33 +0800

    'subscription'

--- source/pipe.js
+++ source/pipe.js
@@ -4,6 +4,27 @@
  * @date 2021-8-17
  */
 
+// SafeSubscriber -》Subscriber -》Subscription 
+ class Subscription{
+    constructor(nextFn){
+        this.nextFn = nextFn;
+    }
+    add(subscription){ // 可能是subscription实例，也可能是函数，也可能是void
+        this.fns.push(subscription)
+    }
+    next(data){
+        this.nextFn(data)
+    }
+    unsubscribe(){
+        this.fns.forEach(fn=>{
+            if(fn instanceof Subscription)
+                fn.unsubscribe();
+            else fn();
+        })
+        this.fns = [];
+    }
+ }
+
 class Observable{
     constructor(subscribe){
         this._subscribe = subscribe;
@@ -20,20 +41,21 @@ class Observable{
         return observable;
     }
     subscribe(observerOrNext){
-        const subscriber = {next: observerOrNext};
+        const subscriber = new Subscription(observerOrNext);
         const {operator, source} = this;
-        // 是操作符
-        if(operator){
-            operator.call(subscriber, source)
-        }else{
-            this._subscribe(subscriber);
-        }
+        subscriber.add(operator ? operator.call(subscriber, source) : this._subscribe(subscriber))
+        return subscriber;
     }
 }
 
-const observable = new Observable();
-observable.pipe(map(), map(), map()).subscribe(()=>{}); 
-
+const observable = new Observable((observer)=>{
+    setTimeout(()=>{
+        observer.next(123)
+    }, 5000)
+    // return function unsubscribe(){ console.log("cancel") }
+});
+let subscription = observable.pipe(map(), map()).subscribe((result)=>{ result === 123 }); 
+setTimeout(()=>{subscription.unsubscribe()}, 1000)
 /**
  * 1. 每个map执行返回一个函数接收observable对象，即上一个source
  *      observable.pipe((source) => (observable), (source) => (observable), (source) => (observable)).subscribe(()=>{}); 
--- src/api/operators/multcasting.tsx
+++ src/api/operators/multcasting.tsx
@@ -1,10 +1,9 @@
 // 多播操作符
-import {interval, take, multicast, map, shareReplay, Subject} from 'rxjs'
+import {interval, take, multicast, Observable, Subscription, map, shareReplay, Subject, of} from 'rxjs'
 import {share} from 'rxjs/operators'
 
 
 export default () => {
-
     // connectedObservable 必须手动调用connect才可以开始
 
     // const tick$ = interval(1_000).pipe(take(5), share({connector: () => new Subject }))
@@ -20,11 +19,11 @@ export default () => {
 
     const source = interval(1000)
         .pipe(
-                map((x: number) => {
-                    console.log('Processing: ', x);
-                    return x*x;
-                }),
-                share()
+            map((x: number) => {
+                console.log('Processing: ', x);
+                return x*x;
+            }),
+            share()
         );
     /** noShare
      *  Processing:  0
@@ -51,8 +50,42 @@ export default () => {
             subscription 1:  4
             subscription 1:  4
      */
-    source.subscribe(x => console.log('subscription 1: ', x));
-    source.subscribe(x => console.log('subscription 1: ', x));
+    // source.subscribe(x => console.log('subscription 1: ', x));
+    // source.subscribe(x => console.log('subscription 1: ', x));
+
+
+
+
+
+
+    // const response = new Subject<Observable<Number>>();
+    // const startSendRequest = new Observable(observer => {
+    //     // Subscription 是在订阅Observable时返回的对象，同时我们也可以使用它作为多个订阅的容器，当它被取消订阅时，其中的所有订阅也将被取消
+    //     // subscriptions 收集 subscribe的返回值（subscription）；
+    //     const subscriptions: Subscription = new Subscription()
+        
+    //     subscriptions.add(
+    //       // 获取响应流
+    //       response.subscribe(result => {
+    //         subscriptions.add(result.subscribe(observer))
+    //       })
+    //     )
+    //     setTimeout(()=>{
+    //         response.next(of(123))
+    //     }, 3000)
+  
+    //     // 当 fetch<T>这个函数返回的Observable发生unsubscribed/errored/completed/canceled,这个返回函数将会被调用取消订阅
+    //     return function unsubscribe() {
+    //         console.log('cancel');
+    //         subscriptions.unsubscribe()
+    //     }
+    // })
+    // let subscription = startSendRequest.subscribe(data => {
+    //     console.log(data);
+    // })
+    // setTimeout(()=>{
+    //     subscription.unsubscribe();
+    // }, 1000)
 
     return <>Hello RxJS</>
 }
